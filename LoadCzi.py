ec0bcb6917156f00cd9df1fe401fab71890c4903
DESKTOP-S6VJFGE\johan <johannes.mueller88@hotmail.de>
2021-07-07 14:41:31 +0200

    no message

--- LoadCzi.py
+++ LoadCzi.py
@@ -10,8 +10,12 @@ Created on Wed Jun 23 12:36:40 2021
 
 import napari
 from aicsimageio import AICSImage
+import os
 
-with napari.gui_qt():
+HE = AICSImage(os.path.join(os.getcwd(), 'ImgData', 'N195e_SAS_28_200302-Scene-2-ScanRegion1_HE.czi'))
     
-    img = AICSImage(r'D:\Documents\Promotion\Projects\2021_MicroQuant\ImgData\N195e_SAS_28_200302-Scene-2-ScanRegion1_HE.czi')
-    napari.view_image(img.data)  # in memory
+
+viewer = napari.Viewer()
+viewer.add_image(HE.dask_data)
+
+napari.run()
